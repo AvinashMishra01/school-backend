010d5580b948481594ea084b122fb4e80af3082f
Avinash Mishra <99175978+AvinashMishra01@users.noreply.github.com>
2025-12-04 02:48:08 +0530

    Remove commented code and clean up school service

--- src/services/school.service.js
+++ src/services/school.service.js
@@ -1,35 +1,3 @@
-// import pool from '../database/db.js'
-
-// export async function createSchoolService(data) {
-//   const query = `
-//     INSERT INTO schools (name, address, city, state, contact, email_id, image)
-//     VALUES (?, ?, ?, ?, ?, ?, ?)
-//   `;
-
-//   const values = [
-//     data.name,
-//     data.address,
-//     data.city,
-//     data.state,
-//     data.contact,
-//     data.email_id,
-//     data.image,
-//   ];
-
-//   const [result] = await pool.query(query, values);
-//   return result.insertId;
-// }
-
-// export const getSchoolsService = async () => {
-//   const [rows] = await pool.query("SELECT * FROM schools");
-
-//   const updatedRows = rows.map((school) => ({
-//     ...school,
-//     image: `http://localhost:5000/uploads/${school.image}`
-//   }));
-
-//   return updatedRows;
-// };
 
 
 import pool from "../database/db.js";
@@ -53,16 +21,7 @@ export const addSchool = async (data) => {
 };
 
 export const getSchools = async (limit, offset) => {
-//   const query = `
-//     SELECT 
-//       *, 
-//       COUNT(*) OVER() as totalCount
-//     FROM schools
-//     LIMIT ? OFFSET ?;
-//   `;
-  
-//   const [rows] = await pool.query(query, [limit, offset]);
-//   return rows;
+
   console.log("get data call")
   const [rows] = await pool.query(
     'SELECT * FROM schools LIMIT ? OFFSET ?',
@@ -76,6 +35,7 @@ export const getSchools = async (limit, offset) => {
   return { rows, totalCount: count.totalCount };
 
 };
+
 
 
 
