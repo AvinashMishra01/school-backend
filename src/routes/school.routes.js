7443801b591ac7eaccf7913932eaee58bda52bc4
Avinash Mishra <99175978+AvinashMishra01@users.noreply.github.com>
2025-12-04 02:47:07 +0530

    Refactor school routes to use memory storage

--- src/routes/school.routes.js
+++ src/routes/school.routes.js
@@ -1,19 +1,3 @@
-// import express from "express";
-// import multer from "multer";
-// import { addSchool, getAllSchools } from "../controllers/school.controller.js";
-
-// const router = express.Router();
-
- 
-// const upload = multer({
-//   dest: "uploads/",
-// });
-
- 
-// router.post("/add-school", upload.single("image"), addSchool);
-// router.get("/schools", getAllSchools);
-
-// export default router;
 
 
 import express from "express";
@@ -21,9 +5,10 @@ import multer from "multer";
 import { addSchoolController, getSchoolsController } from "../controllers/school.controller.js";
 
 const router = express.Router();
-const upload = multer({ storage: multer.memoryStorage() }); // important
+const upload = multer({ storage: multer.memoryStorage() }); 
 
 router.post("/add-school", upload.single("image"), addSchoolController);
 router.get("/schools", getSchoolsController);
+
 
 export default router;
