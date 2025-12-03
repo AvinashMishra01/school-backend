23884ca36455417295ffbe17befdcc12de9cb9ac
Avinash Mishra <99175978+AvinashMishra01@users.noreply.github.com>
2025-12-04 02:49:44 +0530

    Refactor server.js to clean up imports and structure

--- src/server.js
+++ src/server.js
@@ -4,48 +4,26 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
-// import dotenv from "dotenv";
 import schoolRoutes from "./routes/school.routes.js";
 
 
 
 import path from "path";
 import { fileURLToPath } from "url";
-// dotenv.config();
+
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
-// Serve uploaded images
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-
-// for image access
-// app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("Backend running with MySQL!");
 });
 
 app.use("/", schoolRoutes);
-
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
@@ -53,3 +31,4 @@ app.listen(PORT, () => {
   console.log("server con:", process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME);
 
 });
+
